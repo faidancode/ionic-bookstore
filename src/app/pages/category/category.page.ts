2cbcd556d5d424df5a05c83580f1d27f058ce427
faidancode <faidancode393@gmail.com>
2026-04-09 20:59:20 +0700

    category page

--- src/app/pages/category/category.page.ts
+++ src/app/pages/category/category.page.ts
@@ -1,20 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Component, OnInit, signal } from '@angular/core';
+import {
+  IonContent,
+  IonIcon,
+  IonSpinner,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+} from '@ionic/angular/standalone';
+import { CategoryService } from 'src/app/core/services/category.service';
 
 @Component({
+  standalone: true,
   selector: 'app-category',
   templateUrl: './category.page.html',
   styleUrls: ['./category.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonIcon,
+    IonSpinner,
+    IonInfiniteScroll,
+    IonInfiniteScrollContent,
+  ],
 })
 export class CategoryPage implements OnInit {
+  // Signals untuk state management
+  categories = this.categoryService.categories; // Signal dari service
+  loading = this.categoryService.loading; // Signal dari service
 
-  constructor() { }
+  constructor(public categoryService: CategoryService) {}
 
   ngOnInit() {
+    this.categoryService.fetchAll().subscribe();
   }
 
+  onSearch(event: any) {
+    const query = event.target.value;
+    this.categoryService.fetchAll(1, 10, false, query).subscribe();
+  }
+
+  loadMore(event: any) {
+    this.categoryService
+      .fetchAll(this.categoryService.page() + 1, 10, true)
+      .subscribe({
+        next: () => {
+          event.target.complete();
+          // Jika sudah tidak ada data lagi, disable infinite scroll
+          if (!this.categoryService.hasMore()) {
+            event.target.disabled = true;
+          }
+        },
+        error: () => event.target.complete(),
+      });
+  }
+
+  navigateToCategory(category: any) {
+    // Navigasi ke detail
+  }
 }
